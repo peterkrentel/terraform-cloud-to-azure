8d7464a6560fc21204de56eddf5a2e2281176b35
Peter Krentel <peterkrentel@gmail.com>
2024-02-20 16:28:44 -0600

    exsperiment with vm

--- main.tf
+++ main.tf
@@ -1,47 +1,82 @@
+
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+locals {
+  resource_group="app-grp"
+  location="North Europe"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "app_grp"{
+  name=local.resource_group
+  location=local.location
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_virtual_network" "app_network" {
+  name                = "app-network"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.app_grp.name
+  address_space       = ["10.0.0.0/16"]  
+  depends_on = [
+    azurerm_resource_group.app_grp
+  ]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_subnet" "SubnetA" {
+  name                 = "SubnetA"
+  resource_group_name  = local.resource_group
+  virtual_network_name = azurerm_virtual_network.app_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+  depends_on = [
+    azurerm_virtual_network.app_network
+  ]
+}
+
+resource "azurerm_network_interface" "app_interface" {
+  name                = "app-interface"
+  location            = local.location
+  resource_group_name = local.resource_group
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.SubnetA.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.app_public_ip.id
   }
+
+  depends_on = [
+    azurerm_virtual_network.app_network,
+    azurerm_public_ip.app_public_ip,
+    azurerm_subnet.SubnetA
+  ]
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password      = "P@$$w0rd1234!"
+resource "azurerm_windows_virtual_machine" "app_vm" {
+  name                = "appvm"
+  resource_group_name = local.resource_group
+  location            = local.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "demousr"
+  admin_password      =  azurerm_key_vault_secret.vmpassword.value #"Azure@123"
+  availability_set_id = azurerm_availability_set.app_set.id
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.app_interface.id,
   ]
 
   os_disk {
@@ -52,7 +87,162 @@ resource "azurerm_windows_virtual_machine" "example" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
+
+  depends_on = [
+    azurerm_network_interface.app_interface,
+    azurerm_availability_set.app_set,
+    azurerm_key_vault_secret.vmpassword
+  ]
+}
+
+resource "azurerm_public_ip" "app_public_ip" {
+  name                = "app-public-ip"
+  resource_group_name = local.resource_group
+  location            = local.location
+  allocation_method   = "Static"
+  depends_on = [
+    azurerm_resource_group.app_grp
+  ]
+}
+
+resource "azurerm_managed_disk" "data_disk" {
+  name                 = "data-disk"
+  location             = local.location
+  resource_group_name  = local.resource_group
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 16
+}
+# Then we need to attach the data disk to the Azure virtual machine
+resource "azurerm_virtual_machine_data_disk_attachment" "disk_attach" {
+  managed_disk_id    = azurerm_managed_disk.data_disk.id
+  virtual_machine_id = azurerm_windows_virtual_machine.app_vm.id
+  lun                = "0"
+  caching            = "ReadWrite"
+  depends_on = [
+    azurerm_windows_virtual_machine.app_vm,
+    azurerm_managed_disk.data_disk
+  ]
+}
+
+resource "azurerm_availability_set" "app_set" {
+  name                = "app-set"
+  location            = local.location
+  resource_group_name = local.resource_group
+  platform_fault_domain_count = 3
+  platform_update_domain_count = 3  
+  depends_on = [
+    azurerm_resource_group.app_grp
+  ]
+}
+
+resource "azurerm_storage_account" "appstore" {
+  name                     = "pkappstore4577687"
+  resource_group_name      = "app-grp"
+  location                 = "North Europe"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  #allow_blob_public_access = true
+}
+
+resource "azurerm_storage_container" "data" {
+  name                  = "data"
+  storage_account_name  = "pkappstore4577687"
+  container_access_type = "blob"
+  depends_on=[
+    azurerm_storage_account.appstore
+    ]
+}
+
+# Here we are uploading our IIS Configuration script as a blob
+# to the Azure storage account
+
+resource "azurerm_storage_blob" "IIS_config" {
+  name                   = "IIS_Config.ps1"
+  storage_account_name   = "pkappstore4577687"
+  storage_container_name = "data"
+  type                   = "Block"
+  source                 = "IIS_Config.ps1"
+   depends_on=[azurerm_storage_container.data]
+}
+
+resource "azurerm_virtual_machine_extension" "vm_extension" {
+  name                 = "appvm-extension"
+  virtual_machine_id   = azurerm_windows_virtual_machine.app_vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"
+  depends_on = [
+    azurerm_storage_blob.IIS_config
+  ]
+  settings = <<SETTINGS
+    {
+        "fileUris": ["https://${azurerm_storage_account.appstore.name}.blob.core.windows.net/data/IIS_Config.ps1"],
+          "commandToExecute": "powershell -ExecutionPolicy Unrestricted -file IIS_Config.ps1"     
+    }
+SETTINGS
+
+}
+
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = "app-nsg"
+  location            = local.location
+  resource_group_name = local.resource_group
+
+  security_rule {
+    name                       = "Allow_HTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                 = azurerm_subnet.SubnetA.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+  depends_on = [ azurerm_network_security_group.app_nsg ]
+}
+
+resource "azurerm_key_vault" "app_vault" {
+  name                        = "pkkeyvault420"
+  location                    = local.location
+  resource_group_name         = local.resource_group
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "Get",
+    ]
+    secret_permissions = [
+      "Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set",
+    ]
+    storage_permissions = [
+      "Get",
+    ]
+  }
+  depends_on = [
+    azurerm_resource_group.app_grp
+  ]
+}
+
+# craeate the secret
+resource "azurerm_key_vault_secret" "vmpassword" {
+  name = "vmpassword"
+  value = "Azure@123"
+  key_vault_id = azurerm_key_vault.app_vault.id
+  depends_on = [ azurerm_key_vault.app_vault ]
 }
